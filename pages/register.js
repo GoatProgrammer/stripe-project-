ac4482be76ce52f8f1a86a59eb817beb7e57afd8
GoatProgrammer <infernodia@gmail.com>
2023-01-18 16:35:24 -0500

    sdfsadf

--- pages/register.js
+++ pages/register.js
@@ -8,7 +8,9 @@ const register = () => {
     <div>register</div>
     <div>register</div>
     <div>register</div>
-    <div>register</div>
+    <div>register SDFSDFSF</div>
+    <div>register DFASDFSDF</div>
+    <div>register SDFASDF</div>
     <div>register sfdsdf</div>
     <div>register</div>
     <div>register</div>
